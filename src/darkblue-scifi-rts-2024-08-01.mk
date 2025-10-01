193ccc84b7de3c34f2ba62392b39dd061fade0a5
Florent Monnier <monnier.florent@gmail.com>
2025-10-01 11:22:20 +0200

    fixed the size for the second loader

--- src/darkblue-scifi-rts-2024-08-01.mk
+++ src/darkblue-scifi-rts-2024-08-01.mk
@@ -1,7 +1,7 @@
 all: darkblue-scifi-rts-2024-08-01_1.bmp darkblue-scifi-rts-2024-08-01_1.png darkblue-scifi-rts-2024-08-01_1.txt
 darkblue-scifi-rts-2024-08-01_1.bmp: darkblue-scifi-rts-2024-08-01_1.png
 	# Convert to .bmp for sdl3, with a color-key "#ff00ff"
-	convert -size 63x65 'xc:#ff00ff' '(' $< ')' -compose Over -composite $@
+	convert -size 512x547 'xc:#ff00ff' '(' $< ')' -compose Over -composite $@
 darkblue-scifi-rts-2024-08-01_1.png:
 	wget -O - https://raw.githubusercontent.com/fccm2/darkblue-scifi-rts-2024-08-01/refs/heads/result/atlas-2025-05-23_1.png > $@
 darkblue-scifi-rts-2024-08-01_1.txt:
